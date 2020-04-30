893b1f707e40a81e109ba1cb389fabed93606cbc
Bader B <Badoorxp@users.noreply.github.com>
2020-04-30 15:32:14 +0300

    1.8 + 1.9 unicafe step 3 + step 4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 
 const Headings= ({text}) => <h1>{text}</h1>
 
-const Buttons=({text,update})=>{
+const Buttons=({text,update,setShown})=>{
 return(
   <button onClick={update}>{text}</button>
   )  
 }
 
-const Stats=({text,count})=><div>{text} {count}</div>
+const Stats=({text,count})=>{
+  return(<div>{text} {count}</div>)}
 
 const Average=({text,count})=>{
   const calculate=(count)=>count===0?0:(3/count);
@@ -21,6 +22,24 @@ const Average=({text,count})=>{
 
 const Percentage=({text,count,total})=><div>{text} {(count/total)*100}%</div>
 
+const Display=({total,good,bad,neutral})=>{
+  if(total>0)
+  {
+    return(
+      <>
+    <Stats text='Good' count={good}/>
+    <Stats text='Neutral' count={neutral}/>
+    <Stats text='Bad' count={bad}/>
+    <Stats  text='All' count={total}/>
+    <Average  text='Average' count={(good-bad)}/>
+    <Percentage text='Positive' count={good} total={total}/>
+    </>
+    )
+  }
+  return(
+    <div>No Feedback Given</div>
+  )
+}
 
 const App =() =>{
   const [good,setGood]=useState(0)
@@ -35,12 +54,7 @@ const App =() =>{
       <Buttons text='Neutral' update={()=>setNeutral(neutral+1)}/>
       <Buttons text='Bad' update={()=>setBad(bad+1)}/>
       <Headings text='Statistics'/>
-      <Stats text='Good' count={good}/>
-      <Stats text='Neutral' count={neutral}/>
-      <Stats text='Bad' count={bad}/>
-      <Stats  text='All' count={total}/>
-      <Average  text='Average' count={(good-bad)}/>
-      <Percentage text='Positive' count={good} total={total}/>
+      <Display total={total} good={good}  neutral={neutral} bad={bad} />
     </div>
   )
 }
