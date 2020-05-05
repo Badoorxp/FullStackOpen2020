a6147ea3d9005c670cd27bfa4de9ca281c14baf7
Bader B <Badoorxp@users.noreply.github.com>
2020-05-05 04:43:33 +0300

    *After Mapping Commit

--- part1/courseinformation/src/index.js
+++ part1/courseinformation/src/index.js
@@ -2,27 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // const course = {
-  //   id: 1,
-  //   name: 'Half Stack application development',
-  //   parts: [
-  //     {
-  //       name: 'Fundamentals of React',
-  //       exercises: 10,
-  //       id: 1
-  //     },
-  //     {
-  //       name: 'Using props to pass data',
-  //       exercises: 7,
-  //       id: 2
-  //     },
-  //     {
-  //       name: 'State of a component',
-  //       exercises: 14,
-  //       id: 3
-  //     }
-  //   ]
-  // }
   const courses = [
     {
       name: 'Half Stack application development',
@@ -82,17 +61,22 @@ const Course = ({courses}) => {
       <div>
       <Header course={courses}/>
       <Content course={courses}/>
-      {/* <Total parts={course}/> */}
+      {/* <Total parts={courses}/> */}
     </div>
   )
 }
 const Header = ({course}) =>{
-  return (course.map(heading =><h1 key={heading.id}>{heading.name}</h1>))
+  return (
+    course.map((heading) =><h1 key={heading.id}>{heading.name}</h1>))
 }
 
-const Content = ({course}) =>{
-  return (course.map(part=><p key={part.parts.id}>{part.parts.name} {part.parts.exercises}</p>))
-}
+const Content = ({course})=>{
+  return(
+    course.map(courses=>courses.parts.map(part=><p key={part.id}>{part.name} {part.exercises}</p>))
+)}
+  // let heading=course.map(heading=><h1 key={heading.id}>{heading.name}</h1>)
+  // let contents=course.map(heading=>heading.parts.map(part=><p key={part.id}>{part.name} {part.exercises}</p>))
+  
 
 // const Total = ({parts})=><p>Total of {parts.reduce((sum,part)=>sum+part.exercises,0)} exercises.</p>
 
